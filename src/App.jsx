f49d871448ff9888517cd47b415a18e128b168e1
Hashini-Bhagya <kavindihashini15@gmail.com>
2025-09-10 01:02:37 +0530

    todo-delete done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   const [todoText, setTodoText] = useState('');
   const [todos, setTodos] = useState([]);
 
+  const handleRemoveTodo = (itemToRemove) => {
+      setTodos(todos.filter(todo => todo !== itemToRemove));
+  };
+
   const handleAddTodo = () => {
     if (todoText.trim() !== '') {
       setTodos([...todos, todoText]);
@@ -18,7 +22,7 @@ function App() {
     <div>
       <h1>Todo List</h1>
       <Input text={todoText} setText={setTodoText} onAddTodo={handleAddTodo} />
-      <List items={todos} />
+      <List items={todos} setItems={setTodos} />
     </div>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,26 @@
-function List({ items }) {
+function List(props) {
   return (
     <div>
-      {items.length === 0 ? (
-        <p>No items to display. Add one above!</p>
-      ) : (
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      )}
+     {props.items.map((item) => {
+        return (
+            <div key={item} style={{display: "flex"}}>
+                <div>{item}</div>
+                <button 
+                onClick={() => {
+                    const filteredItem = props.items.filter((fitem) => {
+                        if (fitem !== item) {
+                            return true;
+                        }
+                        return false;
+                    });
+
+                    props.setItems(filteredItem);
+                }}>delete</button>
+
+            </div>
+        )
+     })}
+     
     </div>
   );
 }
